2c6069ec37f70c9e1568f3ecbc66960bc22368e8
Thomas Whitaker <trwhitaker01@gmail.com>
2017-10-29 09:17:56 +1100

    28/10

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("connected")
+$('.owl-carousel').owlCarousel({
+	loop: true,
+	margin: 10,
+  nav: true,
+  dots: false,
+	responsive: {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 2
+		},
+		1000: {
+			items: 2
+		}
+	}
+});
+
+document.querySelector(".owl-next").innerHTML = "Next >";
+document.querySelector(".owl-prev").innerHTML = "< Previous";
